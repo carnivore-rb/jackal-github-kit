1ed86e09aa8ef17c95eb6cb56449780ada7909e3
Chris Roberts <code@chrisroberts.org>
2015-05-04 09:45:04 -0700

    Add service registration information

--- lib/jackal-github-kit.rb
+++ lib/jackal-github-kit.rb
@@ -7,3 +7,13 @@ module Jackal
 end
 
 require 'jackal-github-kit/version'
+
+Jackal.service(
+  :github_kit,
+  :description => 'Interact with the GitHub API',
+  :configuration => {
+    :github__access_token => {
+      :description => 'GitHub access token to use for API access'
+    }
+  }
+)
